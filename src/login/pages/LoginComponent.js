128cb505f3ca31224ec30fbd3f48d7a4f650f59e
2CTO <u201521691@upc.edu.pe>
2023-04-13 13:54:39 -0500

    register component has been implemented

--- src/login/pages/LoginComponent.js
+++ src/login/pages/LoginComponent.js
@@ -7,6 +7,7 @@ import megapet from "../../shared/img/megapet.png";
 import {Button} from "primereact/button";
 import { Toast } from 'primereact/toast';
 import {useNavigate} from "react-router-dom";
+import RegisterComponent from "../../shared/components/RegisterComponent";
 const LoginComponent = () => {
     const toast = useRef(null);
     const navigate = useNavigate();
@@ -36,50 +37,58 @@ const LoginComponent = () => {
         }
         })
     return(
-        <div className="card">
-            <Toast ref={toast} />
-            <div className="flex align-content-center justify-content-center mt-8 ">
-                <div className="  mt-8  border-round  w-8 sm:w-2">
-                    <div className="flex flex-column text-center mb-3 mt-8">
-                        <Image src={megapet} width={250} />
-                        <span className="text-600 font-medium line-height-3">Don't have an account?</span>
-                        <a className="font-medium no-underline ml-2 text-blue-500 cursor-pointer" onClick={() => setVisible(false)}>Create now!</a>
-                    </div>
-                    <div className="">
-                        <form onSubmit={formik.handleSubmit}>
-                            <div className="flex flex-column align-items-center justify-content-center">
-                                <div className="p-inputgroup flex-1">
-                                    <span className="p-inputgroup-addon">
-                                        <i className="pi pi-user"></i>
-                                    </span>
-                                    <InputText
-                                        id="email"
-                                        name="email"
-                                        type="text"
-                                        placeholder="Username"
-                                        {...formik.getFieldProps("email")}
-                                    />
+        <>
+            {
+                visible
+                ? (
+                        <div className="card">
+                            <Toast ref={toast} />
+                            <div className="flex align-content-center justify-content-center mt-8 ">
+                                <div className="surface-card  shadow-2 mt-8 p-3  border-round  w-10 sm:w-2">
+                                    <div className="flex flex-column text-center mb-3 mt-4">
+                                        <Image src={megapet} width={250} />
+                                        <span className="text-600 font-medium line-height-3">Don't have an account?</span>
+                                        <a className="font-medium no-underline ml-2 text-blue-500 cursor-pointer" onClick={() => setVisible(false)}>Create now!</a>
+                                    </div>
+                                    <div className="">
+                                        <form onSubmit={formik.handleSubmit}>
+                                            <div className="flex flex-column align-items-center justify-content-center">
+                                                <div className="p-inputgroup flex-1">
+                                                    <span className="p-inputgroup-addon">
+                                                        <i className="pi pi-user"></i>
+                                                    </span>
+                                                    <InputText
+                                                        id="email"
+                                                        name="email"
+                                                        type="text"
+                                                        placeholder="Username"
+                                                        {...formik.getFieldProps("email")}
+                                                    />
+                                                </div>
+                                                <div className="p-inputgroup flex-1 mt-2 ">
+                                                    <span className="p-inputgroup-addon">
+                                                        <i className="pi pi-key"></i>
+                                                    </span>
+                                                    <InputText
+                                                        id="password"
+                                                        name="password"
+                                                        type="password"
+                                                        placeholder="Password"
+                                                        {...formik.getFieldProps("password")}
+                                                    />
+                                                </div>
+                                                <Button type="submit" label="Submit" className=" mt-3"/>
+                                            </div>
+                                        </form>
+                                    </div>
                                 </div>
-                                <div className="p-inputgroup flex-1 mt-2 ">
-                                    <span className="p-inputgroup-addon">
-                                        <i className="pi pi-key"></i>
-                                    </span>
-                                    <InputText
-                                        id="password"
-                                        name="password"
-                                        type="password"
-                                        placeholder="Password"
-                                        {...formik.getFieldProps("password")}
-                                    />
-                                </div>
-                                <Button type="submit" label="Submit" className=" mt-2"/>
                             </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-
+                        </div>
+                    ) : (
+                        <RegisterComponent setVisible={setVisible}/>
+                    )
+            }
+        </>
     );
 }
 
--- src/shared/components/RegisterComponent.js
+++ src/shared/components/RegisterComponent.js
@@ -1,22 +1,18 @@
 import React, {useRef} from "react";
 import {useNavigate} from "react-router-dom";
+import {classNames} from "primereact/utils";
 import UsersApiService from "../../user/services/users-api.service";
 import {useFormik} from "formik";
 import * as Yup from 'yup';
 import {InputText} from "primereact/inputtext";
 import {Button} from "primereact/button";
+import {Image} from "primereact/image";
+import megapet from "../img/megapet.png";
+import {Toast} from "primereact/toast";
 
-const RegisterComponent = () => {
+const RegisterComponent = ({setVisible}) => {
     const toast = useRef(null);
     const navigate = useNavigate();
-
-    const registerFunction = async (values) => {
-        await UsersApiService.createUser(values)
-            .then(() => {
-                window.alert("Close and log in to enjoy eRentCar");
-            })
-            .catch( e => console.log(e));
-    }
 
     const formik = useFormik({
         initialValues: {
@@ -30,15 +26,14 @@ const RegisterComponent = () => {
         validationSchema: Yup.object({
             name: Yup.string()
                 .required("Name is required")
-                .min('Put more than 5 letters'),
+                .min(5, 'Put more than 5 letters'),
             lastName: Yup.string()
                 .required("Last Name is required")
-                .min('Put more than 5 letters'),
+                .min(5, 'Put more than 5 letters'),
             phone: Yup.number()
                 .required("Phone is required"),
             image: Yup.string()
-                .required("Image is required")
-                .min("Put more than 15 letters"),
+                .required("Image is required"),
             email: Yup.string()
                 .required("Email is required"),
             password: Yup.string()
@@ -47,7 +42,9 @@ const RegisterComponent = () => {
         onSubmit: (values) => {
             UsersApiService.createUser(values)
                 .then(response => {
+                    toast.current.show({severity:'success', summary: '!Registration Complete!', detail:'Enjoy MegaPet. Thank you!', life: 3000});
                     console.log(response);
+                    setVisible(true);
                 })
                 .catch(e => console.log(e));
         }
@@ -61,76 +58,89 @@ const RegisterComponent = () => {
 
     return (
       <>
-          <div className="card flex align-items-center justify-content-center">
-              <form onSubmit={formik.handleSubmit}>
-                    <span className="p-float-label ">
-                      <InputText
-                          id="name"
-                          name="name"
-                          type="text"
-                          className={classNames({ 'p-invalid': isFormFieldInvalid("name")})}
-                          {...formik.getFieldProps("name")}
-                      />
-                      <label htmlFor="name">Name</label>
-                  </span>
-                  { getFormErrorMessage('name') }
-                  <span className="p-float-label ">
-                      <InputText
-                          id="lastName"
-                          name="lastName"
-                          type="text"
-                          className={classNames({ 'p-invalid': isFormFieldInvalid("lastName")})}
-                          {...formik.getFieldProps("lastName")}
-                      />
-                      <label htmlFor="Last Name">Last Name</label>
-                  </span>
-                  { getFormErrorMessage('lastName') }
-                  <span className="p-float-label ">
-                      <InputText
-                          id="phone"
-                          name="phone"
-                          type="number"
-                          className={classNames({ 'p-invalid': isFormFieldInvalid("phone")})}
-                          {...formik.getFieldProps("phone")}
-                      />
-                      <label htmlFor="phone">Phone</label>
-                  </span>
-                  { getFormErrorMessage('phone') }
-                  <span className="p-float-label ">
-                      <InputText
-                          id="image"
-                          name="image"
-                          type="text"
-                          className={classNames({ 'p-invalid': isFormFieldInvalid("image")})}
-                          {...formik.getFieldProps("image")}
-                      />
-                      <label htmlFor="image">Image URL</label>
-                  </span>
-                  { getFormErrorMessage('image') }
-                  <span className="p-float-label ">
-                      <InputText
-                          id="email"
-                          name="email"
-                          type="text"
-                          className={classNames({ 'p-invalid': isFormFieldInvalid("email")})}
-                          {...formik.getFieldProps("email")}
-                      />
-                      <label htmlFor="email">Email</label>
-                  </span>
-                  { getFormErrorMessage('email') }
-                  <span className="p-float-label ">
-                      <InputText
-                          id="password"
-                          name="password"
-                          type="text"
-                          className={classNames({ 'p-invalid': isFormFieldInvalid("password")})}
-                          {...formik.getFieldProps("password")}
-                      />
-                      <label htmlFor="password">Password</label>
-                  </span>
-                  { getFormErrorMessage('password') }
-                  <Button label="Submit" type="submit" className="m-5" />
-              </form>
+          <div className="card">
+              <Toast ref={toast} position="center" />
+              <div className="flex align-content-center justify-content-center mt-8">
+                  <div className="surface-card  shadow-2 mt-8 p-3 border-round w-10 sm:w-2">
+                      <div className="flex flex-column text-center justify-content-center align-content-center mb-3 mt-4">
+                          <Image src={megapet} width={250} />
+                          <h4>Complete with your personal information to enjoy MegaPet</h4>
+                      </div>
+                      <div className="">
+                          <form onSubmit={formik.handleSubmit}>
+                              <div className="flex flex-column align-items-center justify-content-center">
+                                  <span className="p-float-label ">
+                                    <InputText
+                                        id="name"
+                                        name="name"
+                                        type="text"
+                                        className={classNames({ 'p-invalid': isFormFieldInvalid("name")})}
+                                        {...formik.getFieldProps("name")}
+                                    />
+                                    <label htmlFor="name">Name</label>
+                                    </ span>
+                                    { getFormErrorMessage('name') }
+                                  <span className="p-float-label ">
+                                    <InputText
+                                        id="lastName"
+                                        name="lastName"
+                                        type="text"
+                                        className={classNames({ 'p-invalid': isFormFieldInvalid("lastName")})}
+                                        {...formik.getFieldProps("lastName")}
+                                    />
+                                      <label htmlFor="Last Name">Last Name</label>
+                                    </ span>
+                                    { getFormErrorMessage('lastName') }
+                                  <span className="p-float-label ">
+                                    <InputText
+                                        id="phone"
+                                        name="phone"
+                                        type="number"
+                                        className={classNames({ 'p-invalid': isFormFieldInvalid("phone")})}
+                                        {...formik.getFieldProps("phone")}
+                                    />
+                                    <label htmlFor="phone">Phone</label>
+                                  </span>
+                                  { getFormErrorMessage('phone') }
+                                  <span className="p-float-label ">
+                                      <InputText
+                                        id="image"
+                                        name="image"
+                                        type="text"
+                                        className={classNames({ 'p-invalid': isFormFieldInvalid("image")})}
+                                        {...formik.getFieldProps("image")}
+                                    />
+                                    <label htmlFor="image">Image URL</label>
+                                  </span>
+                                  { getFormErrorMessage('image') }
+                                  <span className="p-float-label ">
+                                    <InputText
+                                        id="email"
+                                        name="email"
+                                        type="text"
+                                        className={classNames({ 'p-invalid': isFormFieldInvalid("email")})}
+                                        {...formik.getFieldProps("email")}
+                                    />
+                                    <label htmlFor="email">Email</label>
+                                  </span>
+                                  { getFormErrorMessage('email') }
+                                  <span className="p-float-label ">
+                                    <InputText
+                                        id="password"
+                                        name="password"
+                                        type="text"
+                                        className={classNames({ 'p-invalid': isFormFieldInvalid("password")})}
+                                        {...formik.getFieldProps("password")}
+                                    />
+                                    <label htmlFor="password">Password</label>
+                                  </span>
+                                  { getFormErrorMessage('password') }
+                                  <Button label="Submit" type="submit" className="m-3" />
+                              </div>
+                          </form>
+                      </div>
+                  </div>
+                  </div>
           </div>
       </>
     );
